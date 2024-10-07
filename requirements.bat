242d71b56b9a08812469db4b25a4f28e37989926
Programing Kun <phucoding286@gmail.com>
2024-10-07 23:00:50 +0700

    Add files via upload

---
+++ requirements.bat
@@ -0,0 +1,3 @@
+@echo off
+pip install selenium webdriver-manager emoji paramiko scikit-learn python-whois requests bs4 huggingface_hub Pillow tensorflow
+pause
---
+++ requirements.sh
@@ -0,0 +1,1 @@
+pip pip install selenium webdriver-manager emoji paramiko scikit-learn python-whois requests bs4 huggingface_hub Pillow tensorflow
